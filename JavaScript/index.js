47270306e218679fbe8246fe0feaff4f016abb49
jitesh9311 <jiteshwar9311@gmail.com>
2022-07-11 12:49:44 +0530

    Udaya

---
+++ JavaScript/index.js
@@ -0,0 +1,54 @@
+let animeBox1 = document.getElementsByClassName('hC1')[0];
+let animeBox2 = document.getElementsByClassName('hC2')[0];
+let animeBox3 = document.getElementsByClassName('hC3')[0];
+let animeBox4 = document.getElementsByClassName('hC4')[0];
+
+animeBox1.onmouseenter = function() {
+    animeBox1.classList.remove('deActiveHover');
+    animeBox1.classList.add('activeHover');
+}
+
+animeBox1.onmouseleave = function() {
+    animeBox1.classList.remove('activeHover');
+    animeBox1.classList.add('deActiveHover');
+}
+
+////////////////////////////////
+
+animeBox2.onmouseenter = function() {
+    animeBox2.classList.remove('deActiveHover');
+    animeBox2.classList.add('activeHover');
+}
+
+animeBox2.onmouseleave = function() {
+    animeBox2.classList.remove('activeHover');
+    animeBox2.classList.add('deActiveHover');
+}
+
+////////////////////////////////
+
+animeBox3.onmouseenter = function() {
+    animeBox3.classList.remove('deActiveHover');
+    animeBox3.classList.add('activeHover');
+}
+
+animeBox3.onmouseleave = function() {
+    animeBox3.classList.remove('activeHover');
+    animeBox3.classList.add('deActiveHover');
+}
+
+////////////////////////////////
+
+animeBox4.onmouseenter = function() {
+    animeBox4.classList.remove('deActiveHover');
+    animeBox4.classList.add('activeHover');
+}
+
+animeBox4.onmouseleave = function() {
+    animeBox4.classList.remove('activeHover');
+    animeBox4.classList.add('deActiveHover');
+}
+
+
+/////////////////////////////////////////////////////////////////////
+
